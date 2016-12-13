9ea3a5ecb53f8550b6d4ea8a3670cb1ff9bd8134
Jiabin95 <ongjiabin@gmail.com>
2016-12-13 11:31:32 +0800

    first
    ihouse

---
+++ A Pen by alex_files/home.js
@@ -0,0 +1,15 @@
+<script src="./stopExecutionOnTimeout-58d22c749295bca52f487966e382a94a495ac103faca9206cbd160bdf8aedf2a.js.download"></script>
+<script>$(document).ready(function ($) {
+    $('#mgmenu1').universalMegaMenu({
+        menu_effect: 'hover_fade',
+        menu_speed_show: 300,
+        menu_speed_hide: 200,
+        menu_speed_delay: 150,
+        menu_click_outside: true,
+        menubar_trigger: false,
+        menubar_hide: false,
+        menu_responsive: false
+    });
+});
+//# sourceURL=pen.js
+</script>
