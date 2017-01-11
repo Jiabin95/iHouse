e9181706191b180ad7663126ccbb2eebd961e5f0
jiabin95 <ongjiabin@gmail.com>
2017-01-11 15:34:04 +0000

    fdfsds

--- myapp/models.py
+++ myapp/models.py
@@ -8,9 +8,9 @@ class Post(models.Model):
 	text = models.CharField(max_length=30)
 class IMG(models.Model):
     img = models.ImageField(upload_to='upload')
-    room = models.CharField(max_length=30)
-    tit = models.CharField(max_length=30)
-    des = models.CharField(max_length=200)
+    room = models.CharField(max_length=20)
+    tit = models.CharField(max_length=300)
+    des = models.CharField(max_length=100000000000000000)
     
 class Contact(models.Model):
     """ For other types of fields for different purpose, please refer to: https://docs.djangoproject.com/ja/1.10/ref/models/fields/ """
